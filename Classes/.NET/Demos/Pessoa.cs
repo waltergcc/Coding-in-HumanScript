16ffa8fdd497fae0be8ebb83776f87d486684d78
WalterG <waltergccruz@gmail.com>
2022-02-08 21:19:06 -0300

    Valores e Referência

---
+++ Classes/.NET/Demos/Pessoa.cs
@@ -0,0 +1,6 @@
+public class Pessoa
+{
+    public int Idade {get;set;}
+    public string Nome {get;set;}
+    public string Documento {get;set;}
+}
---
+++ Classes/.NET/Demos/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+
+public class Program
+{
+    static void Demo1()
+    {
+        int a = 2;
+        a = Adicionar20(a);
+        WriteLine($"O valor da variável a é {a}");
+    }
+    static int Adicionar20(int a)
+    {
+        return a + 20;
+    }
+
+    static void TrocarNome(Pessoa p1, string nomeNovo)
+    {
+        p1.Nome = nomeNovo;
+    }
+    public static void main()
+    {
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Walter";
+        p1.Idade = 29;
+        p1.Documento = "9875";
+
+        TrocarNome(p1, "José");
+
+        WriteLine($"O novo nome é: {p1.Nome}");
+    }
+}
