392cb36f3665fa7a9e9c511dd3a90bdfa75001d6
WalterG <waltergccruz@gmail.com>
2022-02-12 13:02:04 -0300

    I'm

---
+++ Classes/java/IAm/src/com/study/Main.java
@@ -0,0 +1,13 @@
+package com.study;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final String WALTER = "always learning";
+        while (WALTER == "always learning"){
+            System.out.println("I'm " + WALTER + " in infinite loop!");
+        }
+
+    }
+}
---
+++ Classes/javascript/waltergcc.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+const waltergcc = 'always learning';
+while (waltergcc === 'always learning'){
+    console.log(`I'm ${waltergcc} in infinte loop!`)
+}
+
