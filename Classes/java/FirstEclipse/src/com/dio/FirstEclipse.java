f5ea5f3521134606c56a3e53b3eee07988e8ea0d
WalterG <waltergccruz@gmail.com>
2022-02-12 09:40:15 -0300

    IDE Eclipse

---
+++ Classes/java/FirstEclipse/src/com/dio/FirstEclipse.java
@@ -0,0 +1,15 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class FirstEclipse {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato("Miu", "Amarelo" , 10);
+		
+		System.out.println(gato);
+
+	}
+
+}
