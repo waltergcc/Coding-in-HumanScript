f98c463087617d1b3c37d0bd20e91e0c41c75238
WalterG <waltergccruz@gmail.com>
2022-02-12 11:30:40 -0300

    Usando o intellij

---
+++ Classes/java/FirstIntellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato("Miu", "Amarelo", 10);
+        System.out.println(gato);
+
+    }
+}
