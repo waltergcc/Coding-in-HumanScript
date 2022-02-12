407aa45af1155d0344a088a21c6120e63207e73b
WalterG <waltergccruz@gmail.com>
2022-02-12 13:30:52 -0300

    Commit pelo intellij

--- Classes/java/IAm/src/com/study/Main.java
+++ Classes/java/IAm/src/com/study/Main.java
@@ -4,9 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Commit pelo intellij
         final String WALTER = "always learning";
         while (WALTER == "always learning"){
             System.out.println("I'm " + WALTER + " in infinite loop!");
+
         }
 
     }
