a082fccf325187493e0bddc27123b42d2b21efa0
WalterG <waltergccruz@gmail.com>
2022-02-12 10:05:10 -0300

    Calculadora em Java

---
+++ Classes/java/FirstEclipse/src/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,45 @@
+package com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int a, b;
+
+		System.out.println("Digite um valor:");
+		a = scan.nextInt();
+		
+		System.out.println("Digite outro valor:");
+		b = scan.nextInt();
+		
+		int soma = soma(a ,b);
+		int subtracao = subtracao (a, b);
+		int multiplicacao = multiplicacao(a ,b);
+		float divisao = divisao(a ,b);
+		
+		System.out.println(soma);
+		System.out.println(subtracao);
+		System.out.println(multiplicacao);
+		System.out.println(divisao);
+	}
+
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+
+	public static int subtracao(int a, int b) {
+		return a - b;
+	}
+
+	public static int multiplicacao(int a, int b) {
+		return a * b;
+	}
+
+	public static float divisao(float a, float b) {
+		return a / b;
+	}
+
+}
