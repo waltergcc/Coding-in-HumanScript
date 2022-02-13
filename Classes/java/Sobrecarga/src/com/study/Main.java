789d6a76b7770a66405ab4b25a146a2d39881f2b
WalterG <waltergccruz@gmail.com>
2022-02-12 22:39:24 -0300

    Aula de sobrecarga

---
+++ Classes/java/Sobrecarga/src/com/study/Main.java
@@ -0,0 +1,12 @@
+package com.study;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Exercicio do Quadrilatero:");
+        Quadrilatero.area(5);
+        Quadrilatero.area(5d, 8d);
+        Quadrilatero.area(5, 6, 7);
+        Quadrilatero.area(6f, 6f);
+    }
+}
